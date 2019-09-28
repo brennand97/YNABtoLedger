8fc31ef9ff7e93d8bf43fd4dfe5416101b1bf4bb
Brennan Douglas <brennand97@gmail.com>
2019-09-28 14:15:26 -0700

    fixed pull ynab script

--- pull_ynab.sh
+++ pull_ynab.sh
@@ -6,18 +6,11 @@ FILE=ynab.ledger
 GIT_SSH=true
 GIT_SSH_KEY=~/.ssh/github
 
-pushd $DIR
+retrieveYNAB() {
 
-if ! [ [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1 ]; then
-    echo "Initializing git repo: $DIR"
-    git init
-fi
-
-# Check for 
-if git diff-index --quiet HEAD --; then
     datetime=$(date +"%Y-%m-%d %T")
     ynab-to-ledger > $FILE
-    if [ $? -nq 0 ]; then
+    if [ $? -ne 0 ]; then
         >&2 echo "Failed to run 'ynab-to-ledger'"
     fi
     git add $FILE
@@ -32,6 +25,18 @@ if git diff-index --quiet HEAD --; then
         
         git push
     fi
+
+}
+
+pushd $DIR
+
+[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Initializing git repo: $DIR"
+    git init
+    retrieveYNAB
+elif git diff-index --quiet HEAD --; then
+    retrieveYNAB
 else
     >&2 echo "$DIR is dirty, there is uncommited changes.  Exiting..."
     popd
