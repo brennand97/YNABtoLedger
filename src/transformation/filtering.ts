c870231c91b226fdde41c6b6f997310f75278ea4
Brennan Douglas <brennand97@gmail.com>
2019-11-29 22:13:08 -0800

    add multi-line account regex filtering

--- src/transformation/filtering.ts
+++ src/transformation/filtering.ts
@@ -7,9 +7,14 @@ export function filterEntries(config: IConfiguration, entries: IEntry[]): IEntry
 
     if (config.account_filter && config.account_filter.length > 0) {
 
-        entries = entries.filter(entry => entry.splits
-            .some(split => config.account_filter
-                .some(filter => `${split.group}:${split.account}`.match(filter))));
+        const regexFilter: RegExp[] = config.account_filter.map(filter => new RegExp(filter, 'gm'));
+
+        entries = entries.filter(entry => {
+            const accountNames =  entry.splits
+                .map(split => `${split.group}:${split.account}`)
+                .join('\n');
+            return regexFilter.some(filter => accountNames.match(filter));
+        });
 
     }
 
